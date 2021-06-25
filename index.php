d0fa2dae96c079fdb166eb036042a3333e1a9eaa
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-06-25 13:42:52 +0200

    Mise en place de css

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <script async src="public/js/index.js"></script>
     <title>TODO_PHP</title>
 </head>
 
 <body>
-    <header>HEADER</header>
-    <div class="content">CONTENT</div>
-    <footer>FOOTER</footer>
+    <div class="container">
+        <header>
+            <div class="logo">TODO_PHP</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer class="footer">2021 © Tous droits réservés</footer>
+    </div>
 </body>
 
 </html>
