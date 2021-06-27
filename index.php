c71917d2cc66162f29dd8a1b5c01320d08b7139a
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-06-27 22:42:56 +0200

    Afichage

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 const ERROR_REQUIRED = 'Veuillez sesir un text';
 const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filname = __DIR__ . "/data/todos.json";
 $error = '';
+$todos = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -11,6 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $error = ERROR_REQUIRED;
     } else if (mb_strlen($todo) < 5) {
         $error = ERROR_TOO_SHORT;
+    }
+    // condition sur pour remplir le form
+
+    if (file_exists($filname)) {
+        $data = file_get_contents($filname);
+        $todos = json_decode($data, true) ?? [];
+    }
+
+    //condition sur la vérification et sauvegarde de la nouvelle todo
+
+    if (!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+        ]];
+        file_put_contents($filname, json_encode($todos));
     }
 }
 ?>
@@ -40,7 +59,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <?php if ($error) : ?>
                 <p class="text-danger"><?= $error ?></p>
                 <?php endif; ?>
-                <div class="todo-list"></div>
+
+                <ul class="todo-list"><br>
+                    <?php foreach ($todos as $st) : ?>
+                    <li>
+                        <span class="todo-name"><?= $st['name'] ?> </span>
+                        <button class="btn btn-primary btn-small">Valider</button>
+                        <button class="btn btn-denger btn-small">Supprimer</button>
+                    </li>
+                    <?php endforeach; ?>
+
+                </ul>
             </div>
         </div>
         <?php require_once 'inc/footer.php' ?>
